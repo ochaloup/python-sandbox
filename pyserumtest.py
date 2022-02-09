aa9c48c3acce69d6f26d48a201ed077a63a5e7c9
Ondra Chaloupka <chalda@chainkeepers.io>
2022-02-09 15:56:25 +0100

    show me orderbook pyserum

--- pyserumtest.py
+++ pyserumtest.py
@@ -1,20 +1,33 @@
 # from https://github.com/serum-community/pyserum
 
+import pyserum
 from pyserum.connection import conn
 from pyserum.market import Market
 
-cc = conn("https://api.mainnet-beta.solana.com/")
-market_address = "5LgJphS6D5zXwUVPU7eCryDBkyta3AidrJ5vjNU6BcGW" # Address for BTC/USDC
+# pyserum.get_token_mints()
+# token mint: https://raw.githubusercontent.com/project-serum/serum-ts/master/packages/serum/src/token-mints.json
+# pyserum.get_live_markets()
+# markets: https://raw.githubusercontent.com/project-serum/serum-ts/master/packages/serum/src/markets.json
+
+# consider https://mango.rpcpool.com/946ef7337da3f5b8d3e4a34e7f88
+rpcConnection = conn("https://api.mainnet-beta.solana.com/")
+
+# market_address = "5LgJphS6D5zXwUVPU7eCryDBkyta3AidrJ5vjNU6BcGW" # Address for BTC/USDC
+market_address = "7xMDbYTCqQEcK2aM9LbetGtNFJpzKdfXzLL5juaLh4GJ" # SOL/USDC
 
 # Load the given market
-market = Market.load(cc, market_address)
+print("Loading markets...")
+market = Market.load(rpcConnection, market_address)
+print(f'Market {market}')
+
+print("Loading asks...")
 asks = market.load_asks()
+
 # Show all current ask order
 print("Ask Orders:")
 for ask in asks:
     print("Order id: %d, price: %f, size: %f." % (
           ask.order_id, ask.info.price, ask.info.size))
-
 print("\n")
 # Show all current bid order
 print("Bid Orders:")
